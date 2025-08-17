42f6492d78f3b9018aaba3eb9ceb73da71964109
FThappy <thangchu766@gmail.com>
2025-08-17 21:07:18 +0700

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Portfolio/',
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 });
